fc095e376d97a3a08bd1fda22051e15837f28166
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-01 01:53:20 +0900

    Update command.js

--- command.js
+++ command.js
@@ -2,12 +2,19 @@
 
 const { prefix } = request('./config.json')
 
-['ping', 'test']
+/*
+    ['ping', 'test']
 
+    'ping'
+*/
 
 
 module.exports = (client, aliases, callback) => {
     
+    if(typeof aliases === 'string') {
+        aliases = [aliases]
+    }
+
     client.on('message', message => {
         const { content } = message;
     })
