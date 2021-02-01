a43a5bdef273ec604a39f4455f34de1210cd116f
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-02 07:42:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ const command = require('./command')
 client.on('ready', () => {
     console.log("The Client is Ready")
 
-    command(client, 'hi', message => {
+    command(client, 'hi', (message) => {
         message.channel.send('Hi Kinesys Server!')
     })
 
-    command(client, 'name', message => {
+    command(client, 'name', (message) => {
         message.channel.send("My name is Nemeisys, Discord Bot.")
     })
 
-    command(client, 'name -k', message => {
+    command(client, 'name -k', (message) => {
         message.channel.send("제 이름은 네메시스, 디스코드 봇 입니다.")
+    })
+
+    command(client, 'servers', (message) => {
+        client.guilds.cache.forEach((guild) => {
+            console.log(guild)
+        })
+    })
+
+    command(client, 'members', (message) => {
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name}의 Member는 총 ${guild.memberCount}명 입니다`)
+        })
     })
 
 })
